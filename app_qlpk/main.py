7a76c51d2fbf0c6da0730853bf48ad246309a38d
LeTranVu00 <siculakeomut123@gmail.com>
2025-11-17 04:11:38 +0700

    Delete app_qlpk directory

--- app_qlpk/main.py
+++
@@ -1,9 +0,0 @@
-from login import LoginWindow
-import sys
-from PyQt5.QtWidgets import QApplication
-
-if __name__ == "__main__":
-    app = QApplication(sys.argv)
-    window = LoginWindow()
-    window.show()
-    sys.exit(app.exec_())
